91cf21bd0c03e744418d35c4b5a87405e43dee82
creztfallen <mtssmoreno@gmail.com>
2023-02-15 08:37:40 -0300

    refactor: :art: refactor hello_test.go to stop repeating code

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,25 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	result := hello("Geralt")
-	expected := "Hello, Geralt."
 
-	if result != expected {
-		t.Errorf("Resultado '%s', esperado '%s'", result, expected)
+	verifyCorrectMessage := func(t *testing.T, result, expected string) {
+		t.Helper()
+		if result != expected {
+			t.Errorf("resultado '%s', esperado '%s'", result, expected)
+		}
 	}
+
+	t.Run("say hello to people", func(t *testing.T) {
+		result := Hello("Geralt")
+		expected := "Hello, Geralt."
+
+		verifyCorrectMessage(t, result, expected)
+	})
+
+	t.Run("say 'Hello, world' when an empty string is passed", func(t *testing.T) {
+		result := Hello("")
+		expected := "Hello, world."
+
+		verifyCorrectMessage(t, result, expected)
+	})
 }
