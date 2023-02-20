ac08420c04baca26ebb4b12b67bf9461bdbbc200
Mateus <creztfallen@M.S.M>
2023-02-20 12:08:08 -0300

    feat::sparkles:add structs tests

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -3,10 +3,9 @@ package iteration
 import "strings"
 
 func Repeat(stringToBeRepeated string, numberToRepeat int) string {
-	var repetitions string
 	// for i := 0; i < numberToRepeat; i++ {
 	// 	repetitions += stringToBeRepeated
 	// }
-	repetitions = strings.Repeat(stringToBeRepeated, numberToRepeat)
+	repetitions := strings.Repeat(stringToBeRepeated, numberToRepeat)
 	return repetitions
 }
